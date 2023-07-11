5e21bdf67ac2566cb0cc27a21bf18bb1f80e8cf5
onusra <onusra@angelo.edu>
2023-07-11 23:14:46 +0000

    completed

--- src/main/java/MaxMinusMin.java
+++ src/main/java/MaxMinusMin.java
@@ -12,6 +12,8 @@ public class MaxMinusMin {
     public static void main(String [] args){
         
     }
+
+    
     public int difference(List<Integer> nums){
         int min, max;
         max = Collections.max(nums);
